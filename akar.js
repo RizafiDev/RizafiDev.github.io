37b596b7b6183ba5a73f95c5d92978fe706e3dad
RizafiDev <152231219+RizafiDev@users.noreply.github.com>
2023-11-29 15:43:17 +0000

    Add files via upload

---
+++ akar.js
@@ -0,0 +1,18 @@
+function hitungAkar() {
+  // Mendapatkan nilai dari input
+  var inputAkar = document.getElementById("inputAkar").value;
+  var rootType = document.getElementById("rootType").value;
+
+  // Validasi input
+  if (inputAkar === "" || isNaN(inputAkar)) {
+    alert("Masukkan angka yang valid");
+    return;
+  }
+
+  // Menghitung akar sesuai jenis yang dipilih
+  var hasilAkar = Math.pow(parseFloat(inputAkar), 1 / rootType);
+
+  // Menampilkan hasil
+  document.getElementById("resultAkar").innerHTML =
+    "Akar " + rootType + " dari " + inputAkar + " : " + hasilAkar;
+}
